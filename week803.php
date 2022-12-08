517daf2d2cde82d2d2c5d5dfa718296774787f2b
Aruza354 <Aruza354@gmail.com>
2022-12-08 15:52:57 +0700

    update

---
+++ week803.php
@@ -0,0 +1,27 @@
+<?php
+ function add ($a,$b){
+    return $a + $b;
+}
+function sub($a,$b){
+    return $a - $b;
+}
+function division($a,$b){
+    return $a / $b;
+}
+function muiti($a,$b){
+    return $a * $b;
+}
+function mod ($a,$b){
+    return $a % $b;
+}
+
+   $total = add (10,5);
+   print(<h1>$total</h1>);
+   $total = sub (10,5);
+   print(<h1>$total</h1>);
+   $total = division (10,5);
+   print(<h1>$total</h1>);.
+   $total = muiti (10,5);
+   print(<h1>$total</h1>);
+ 
+ ?>
